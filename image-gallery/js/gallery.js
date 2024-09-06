de25e06cc08d2239449fb7f352a3764adb8c6d33
M1stralll <statkevichmisha@gmail.com>
2024-09-06 22:35:18 +0300

    fix: fix searchImages and style

--- image-gallery/js/gallery.js
+++ image-gallery/js/gallery.js
@@ -1,40 +1,75 @@
 const accessKey = "gb3Ip1vqqcoMsD11w9gMVCao1-Hc-mEFXDIk3PxJ2Cs";
 
 const formEl = document.querySelector("form");
-const inputEl = document.getElementById("search-input");
+const inputEl = document.querySelector(".name-picture");
 const searchResults = document.querySelector(".search-results");
 
-let inputData = "";
-let page = 1;
+
+searchImages("spring");
+
 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
-    searchImages();
+    searchImages(inputEl.value);
 });
 
-async function searchImages() {
-    inputData = inputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+async function searchImages(searchToken) {
     
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${searchToken}&client_id=${accessKey}`;
     const response = await fetch(url);
     const data = await response.json();
+    
 
     const results = data.results; 
 
-    if (page === 1){
         searchResults.innerHTML = "";
-    }
 
     results.map((result) => {
         const imageWrapper = document.createElement('div');
+        
         imageWrapper.classList.add("result");
         const image = document.createElement('img');
         image.src = result.urls.small;
         image.alt = result.alt_description;
-
         imageWrapper.appendChild(image);
         searchResults.appendChild(imageWrapper);
     });
 
-    page++;
 }
+
+
+
+
+
+
+
+
+// async function searchImages() {
+//     const url = `https://api.unsplash.com/search/photos?page=1&query=spring&client_id=${accessKey}`;
+    
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     const results = data.results; 
+
+//         searchResults.innerHTML = "";
+
+//     results.map((result) => {
+//         const imageWrapper = document.createElement('div');
+//         imageWrapper.classList.add("result");
+//         const image = document.createElement('img');
+//         image.src = result.urls.small;
+//         image.alt = result.alt_description;
+
+//         imageWrapper.appendChild(image);
+//         searchResults.appendChild(imageWrapper);
+//     });
+
+// }
+
+// function setFocus()
+// {
+//      document.querySelector(".name-picture").focus();
+// }
+// setFocus()
